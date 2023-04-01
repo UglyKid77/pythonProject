3eaa8dfee4bf527c4f02ef3ba84a044b2ebc7c63
UglyKid77 <gustavodart@yahoo.es>
2023-04-01 16:30:37 +0200

    Chapter1

---
+++ module1-first-program.py
@@ -0,0 +1,62 @@
+
+# print('Hello, suckers')
+# print('You\'r a sucker \nand you too')
+#
+# #### Variables
+#
+# print('...')
+# greeting = 'Hey you'
+# print(greeting)
+#
+# #### Numerical representations
+#
+# print('...')
+# print(0xAF5)
+#
+# ##### Operator
+#
+# print('...')
+# print(56 // 3)
+# print(56 % 3)
+#
+# #### values
+#
+# print('...')
+# text = 'High'+'Power'
+# print(text)
+# print(text*5)
+
+##### input
+#
+# print('...')
+# print('What\'s the Craic?')
+# craic = input()   ### Always return a string value
+# print('Yes,', craic)
+
+#### type-casting
+
+# print('...')
+# char = input('Enter Number A: ')
+# int_char = int(char)    #float()  str()
+# print('30 + A =', int_char + 30)
+
+
+##### more print and string
+
+Name = 'Ratón'
+A = len('hey you')
+print(A)
+print('Hey you', Name, 'My friend', sep='-', end='.')
+print('Where is the cat')
+
+##### bit operators
+
+# & and
+# | or
+# ^ exclusive or
+# ~ negation
+
+
+
+
+
