5b772fd0f9ac4099053b9f39b1b83a78ed734be3
UglyKid77 <gustavodart@yahoo.es>
2023-04-01 17:08:12 +0200

    Chapter2

---
+++ Control Flow.py
@@ -0,0 +1,32 @@
+#### IF
+
+Age = 46
+
+if Age > 46:
+    print('Please come in')
+elif Age == 46:
+    print('Wao')
+else:
+    print('sorry, go back to kindergarten')
+
+# <=  >= !=
+
+##### Operators
+
+# and, or, if not.  braquets ()
+
+#### while
+
+# while condition:
+#     intructions
+# print('finished')
+
+# break, cotinue
+
+#### For
+
+for letter in 'Linux':
+    print('Current letter:', letter)
+
+for counter in range(2,22,3):
+    print(counter)
