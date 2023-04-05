befbc9097bf9fa02bbe6d57be3f4c32a69a3ae19
UglyKid77 <gustavodart@yahoo.es>
2023-04-05 17:00:51 +0200

    5.4.23

--- Functions and Exceptions.py
+++ Functions and Exceptions.py
@@ -32,3 +32,44 @@ def show_truth():
 show_truth()
 print(nam)
 
+### RETURN keyword
+
+def  get_average(nin):
+    sum = 0.0
+    for i in nin:
+        sum += i
+    average = sum / len(nin)
+    return average
+
+A = [5, 4, 6, 10, 7]
+print(get_average(A))
+
+
+### recursion
+
+def get_factorial_recursive(number):
+    if number <= 1:
+        return 1
+    return number * get_factorial_recursive(number -1)
+
+print(get_factorial_recursive(3))
+
+### generators
+
+def get_number():
+    for i in range(1, 4):
+        yield i
+
+
+print(get_number())
+gen = get_number()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+
+for x in get_number():
+    print(x)
+
+numbers = list(get_number())   #creating a list
+print(numbers)
+
